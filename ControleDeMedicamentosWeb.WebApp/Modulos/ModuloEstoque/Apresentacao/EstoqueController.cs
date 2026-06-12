7501e1f610e8745ce067def17f63f2abf2f57e32
DrElucidator <dr.elucidator@gmail.com>
2026-06-12 18:05:06 -0300

    Resolvido bug de recarregamento de entidades

--- ControleDeMedicamentosWeb.WebApp/Modulos/ModuloEstoque/Apresentacao/EstoqueController.cs
+++ ControleDeMedicamentosWeb.WebApp/Modulos/ModuloEstoque/Apresentacao/EstoqueController.cs
@@ -11,29 +11,26 @@ public class EstoqueController(ServicoEstoque servicoEstoque, IMapper mapeador) 
     [HttpGet]
     public ActionResult Listar()
     {
-        List<ListarEntradaDto> entradas = servicoEstoque.SelecionarEntradas();
-        List<ListarSaidaDto> saidas = servicoEstoque.SelecionarSaidas();
+        var entradasVm = mapeador.Map<List<ListarEntradaViewModel>>(servicoEstoque.SelecionarEntradas());
+        var saidasVm = mapeador.Map<List<ListarSaidaViewModel>>(servicoEstoque.SelecionarSaidas());
 
-        List<ListarEntradaViewModel> entradasVm = mapeador.Map<List<ListarEntradaViewModel>>(entradas);
-        List<ListarSaidaViewModel> saidasVm = mapeador.Map<List<ListarSaidaViewModel>>(saidas);
-
-        ListarEstoqueViewModel listarVm = new ListarEstoqueViewModel(entradasVm, saidasVm);
-
-        return View(listarVm);
+        return View(new ListarEstoqueViewModel(entradasVm, saidasVm));
     }
 
     [HttpGet]
     public ActionResult CadastrarEntrada()
     {
-        CadastrarEntradaViewModel vm = new(
-            DateTime.Now,
-            Guid.Empty,
-            Guid.Empty,
-            0,
-            servicoEstoque.SelecionarMedicamentos().Select(m => new OpcaoMedicamentoViewModel(m.Id, m.Nome)).ToList(),
-            servicoEstoque.SelecionarFuncionarios().Select(f => new OpcaoFuncionarioViewModel(f.Id, f.Nome)).ToList()
+        var vm = new CadastrarEntradaViewModel(
+        DateTime.Now,
+        Guid.Empty,
+        Guid.Empty,
+        0,
+        servicoEstoque.SelecionarMedicamentos()
+            .Select(m => new OpcaoMedicamentoViewModel(m.Id, m.Nome)).ToList(),
+        servicoEstoque.SelecionarFuncionarios()
+            .Select(f => new OpcaoFuncionarioViewModel(f.Id, f.Nome)).ToList()
         );
-
+        
         return View(vm);
     }
 
@@ -41,14 +38,39 @@ public class EstoqueController(ServicoEstoque servicoEstoque, IMapper mapeador) 
     public ActionResult CadastrarEntrada(CadastrarEntradaViewModel vm)
     {
         if (!ModelState.IsValid)
-            return View(vm);
+        {
+            vm = new CadastrarEntradaViewModel(
+            vm.Data,
+            vm.MedicamentoId,
+            vm.FuncionarioId,
+            vm.Quantidade,
+            servicoEstoque.SelecionarMedicamentos()
+                .Select(m => new OpcaoMedicamentoViewModel(m.Id, m.Nome)).ToList(),
+            servicoEstoque.SelecionarFuncionarios()
+                .Select(f => new OpcaoFuncionarioViewModel(f.Id, f.Nome)).ToList()
+            );
 
-        CadastrarEntradaDto dto = mapeador.Map<CadastrarEntradaDto>(vm);
-        Result resultado = servicoEstoque.CadastrarEntrada(dto);
+            return View(vm);
+        }
+
+        var dto = mapeador.Map<CadastrarEntradaDto>(vm);
+        var resultado = servicoEstoque.CadastrarEntrada(dto);
 
         if (resultado.IsFailed)
         {
             ModelState.AddModelError(resultado);
+
+            vm = new CadastrarEntradaViewModel(
+            vm.Data,
+            vm.MedicamentoId,
+            vm.FuncionarioId,
+            vm.Quantidade,
+            servicoEstoque.SelecionarMedicamentos()
+                .Select(m => new OpcaoMedicamentoViewModel(m.Id, m.Nome)).ToList(),
+            servicoEstoque.SelecionarFuncionarios()
+                .Select(f => new OpcaoFuncionarioViewModel(f.Id, f.Nome)).ToList()
+            );
+
             return View(vm);
         }
 
@@ -58,12 +80,14 @@ public class EstoqueController(ServicoEstoque servicoEstoque, IMapper mapeador) 
     [HttpGet]
     public ActionResult CadastrarSaida()
     {
-        CadastrarSaidaViewModel vm = new(
-            DateTime.Now,
-            Guid.Empty,
-            new List<MedicamentoSaidaViewModel>(),
-            servicoEstoque.SelecionarPacientes().Select(p => new OpcaoPacienteViewModel(p.Id, p.Nome)).ToList(),
-            servicoEstoque.SelecionarMedicamentos().Select(m => new OpcaoMedicamentoViewModel(m.Id, m.Nome)).ToList()
+        var vm = new CadastrarSaidaViewModel(
+        DateTime.Now,
+        Guid.Empty,
+        new List<MedicamentoSaidaViewModel>(),
+        servicoEstoque.SelecionarPacientes()
+            .Select(p => new OpcaoPacienteViewModel(p.Id, p.Nome)).ToList(),
+        servicoEstoque.SelecionarMedicamentos()
+            .Select(m => new OpcaoMedicamentoViewModel(m.Id, m.Nome)).ToList()
         );
 
         return View(vm);
@@ -73,14 +97,37 @@ public class EstoqueController(ServicoEstoque servicoEstoque, IMapper mapeador) 
     public ActionResult CadastrarSaida(CadastrarSaidaViewModel vm)
     {
         if (!ModelState.IsValid)
-            return View(vm);
+        {
+            vm = new CadastrarSaidaViewModel(
+            vm.Data,
+            vm.PacienteId,
+            vm.Medicamentos,
+            servicoEstoque.SelecionarPacientes()
+                .Select(p => new OpcaoPacienteViewModel(p.Id, p.Nome)).ToList(),
+            servicoEstoque.SelecionarMedicamentos()
+                .Select(m => new OpcaoMedicamentoViewModel(m.Id, m.Nome)).ToList()
+            );
 
-        CadastrarSaidaDto dto = mapeador.Map<CadastrarSaidaDto>(vm);
-        Result resultado = servicoEstoque.CadastrarSaida(dto);
+            return View(vm);
+        }
+
+        var dto = mapeador.Map<CadastrarSaidaDto>(vm);
+        var resultado = servicoEstoque.CadastrarSaida(dto);
 
         if (resultado.IsFailed)
         {
             ModelState.AddModelError(resultado);
+
+            vm = new CadastrarSaidaViewModel(
+            vm.Data,
+            vm.PacienteId,
+            vm.Medicamentos,
+            servicoEstoque.SelecionarPacientes()
+                .Select(p => new OpcaoPacienteViewModel(p.Id, p.Nome)).ToList(),
+            servicoEstoque.SelecionarMedicamentos()
+                .Select(m => new OpcaoMedicamentoViewModel(m.Id, m.Nome)).ToList()
+            );
+
             return View(vm);
         }
 
